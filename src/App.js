18bebbb85b31a58cdd7fc826a42733de73d4e2b3
Silja Ástudóttir <silja.astudottir@gmail.com>
2023-11-16 23:33:26 +0000

    files updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 import Todo from './components/todo';
 
 function App() {
   return (
     <div
-      className="container border border-dark rounded mt-5"
+      className="container border border-light rounded mt-5"
       style={{
-        background: `url(https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700092800&semt=sph)`,
-        backgroundSize: "cover",
+        background: 'url(https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700092800&semt=sph)',
+        backgroundSize: 'cover',
       }}
     >
       <Todo/>
